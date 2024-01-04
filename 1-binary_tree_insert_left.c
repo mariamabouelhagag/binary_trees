6ad3f1b96259c3e28c7b5e43178aa9201cc3159f
mariam salama <mariamsalama888@gmail.com>
2024-01-04 12:37:32 -0800

    set at left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,37 +12,36 @@
  * Return: a pointer to the created node, or NULL on failure or if
  *			parent is NULL
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *binary_node;
+	binary_tree_t *binary_node;
 
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    binary_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    
+	binary_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    if (binary_node == NULL)
-    {
-        return (NULL);
-    }
+	if (binary_node == NULL)
+	{
+		return (NULL);
+	}
 
-    binary_node->n = value;
-    binary_node->parent = parent;
-    binary_node->left = NULL;
-    binary_node->right = NULL;
+	binary_node->n = value;
+	binary_node->parent = parent;
+	binary_node->left = NULL;
+	binary_node->right = NULL;
 
-    if (parent->left != NULL)
-    {
-        binary_node->left = parent->left;
-        parent->left->parent = binary_node;
-    }
-    else
-    {
-        parent->left = binary_node;
-
-    }   
- return (binary_node);
+	if (parent->left != NULL)
+	{
+		binary_node->left = parent->left;
+		parent->left->parent = binary_node;
+	}
+	else
+	{
+		parent->left = binary_node;
+	}
+	return (binary_node);
 }
