f23df611f99ac8281101c39cec831eb2c3829df6
mariam salama <mariamsalama888@gmail.com>
2024-01-04 14:31:25 -0800

    high

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: tree to measure the height of
@@ -7,15 +6,25 @@
  * Return: height of the tree
  *         0 if tree is NULL
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
-
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
+	}
 
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	size_t left_high = binary_tree_height(tree->left);
+	size_t right_high = binary_tree_height(tree->right);
+	size_t max_high;
+
+	if (left_high > right_high)
+	{
+		max_high = left_high;
+	}
+	else
+	{
+		max_high = right_high;
+	}
+	return (max_high + 1);
 }
